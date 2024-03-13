44add5352b92119a876051c218a1a2737050bb53
Yiwen.Chang <yiwen.chang@tpisoftware.com>
2024-03-13 10:46:24 +0800

    initial commit

---
+++ src/main/java/com/yiwen/test/mybatis_practice/MybatisPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.yiwen.test.mybatis_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yiwen/test/mybatis_practice/MybatisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiwen.test.mybatis_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
